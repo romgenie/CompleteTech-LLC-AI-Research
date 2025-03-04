4fa6d75af23801f9a547cac83caa4f66b1828a72
Timothy Gregg <Timothy.Gregg@complete.tech>
2025-03-04 10:31:11 -0500

    feat: Add API routers for the application

---
+++ src/api/routers/__init__.py
@@ -0,0 +1,11 @@
+"""
+API routers for the application.
+"""
+
+from src.api.routers import (
+    auth,
+    health,
+    knowledge_graph,
+    research_orchestration,
+    research_implementation,
+)
