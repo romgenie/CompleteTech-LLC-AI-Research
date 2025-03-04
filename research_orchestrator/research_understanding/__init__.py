3cea194e698a56a7a7f658170911b3c210244448
Timothy Gregg <Timothy.Gregg@complete.tech>
2025-03-04 01:41:35 -0500

    Add ResearchUnderstandingEngine to module exports

--- research_orchestrator/research_understanding/__init__.py
+++ research_orchestrator/research_understanding/__init__.py
@@ -9,7 +9,11 @@ from research papers, including algorithms, implementation details, and architec
 from . import paper_processing
 from . import algorithm_extraction
 
+# Import main engine class
+from .understanding_engine import ResearchUnderstandingEngine
+
 __all__ = [
     'paper_processing',
     'algorithm_extraction',
+    'ResearchUnderstandingEngine',
 ]
