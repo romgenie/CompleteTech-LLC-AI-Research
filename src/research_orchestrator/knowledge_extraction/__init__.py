db848e3f5f2c952b896a07c27dbf4be8c32ba909
Timothy Gregg <Timothy.Gregg@complete.tech>
2025-03-04 06:59:49 -0500

    Refactor imports in knowledge extraction module to include only fully implemented components

--- src/research_orchestrator/knowledge_extraction/__init__.py
+++ src/research_orchestrator/knowledge_extraction/__init__.py
@@ -5,8 +5,21 @@ This module provides components for extracting structured knowledge from various
 information sources, including academic papers, web content, and code repositories.
 """
 
-from research_orchestrator.knowledge_extraction.document_processing import DocumentProcessor
-from research_orchestrator.knowledge_extraction.entity_recognition import EntityRecognizer
-from research_orchestrator.knowledge_extraction.relationship_extraction import RelationshipExtractor
+# Import only components that are fully implemented
+from src.research_orchestrator.knowledge_extraction.document_processing import DocumentProcessor
+
+# Import entity recognition components
+from src.research_orchestrator.knowledge_extraction.entity_recognition import (
+    Entity, 
+    EntityType,
+    EntityRecognizer, 
+    AIEntityRecognizer,
+    ScientificEntityRecognizer,
+    EntityRecognizerFactory,
+    CombinedEntityRecognizer
+)
+
+# Relationship extraction will be imported when implemented
+# from src.research_orchestrator.knowledge_extraction.relationship_extraction import RelationshipExtractor
 
 __version__ = "0.1.0"
