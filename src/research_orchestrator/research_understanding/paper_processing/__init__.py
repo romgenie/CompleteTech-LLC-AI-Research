10eae103b903599961e699bc93b543524f1efe43
Timothy Gregg <Timothy.Gregg@complete.tech>
2025-03-04 09:54:38 -0500

    feat: Add Paper Processing module for extracting information from research papers

---
+++ src/research_orchestrator/research_understanding/paper_processing/__init__.py
@@ -0,0 +1,5 @@
+"""
+Paper Processing module for extracting information from research papers.
+"""
+
+from .paper_processor import PaperProcessor, StructuredPaper, PaperFormat, PaperSection, PaperReference, PaperFigure, PaperTable, PaperAlgorithm
