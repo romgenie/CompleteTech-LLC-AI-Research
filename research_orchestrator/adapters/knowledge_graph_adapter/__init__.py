a96a3625e85524def9489b377ba8c2c6b04e76d4
Timothy Gregg <Timothy.Gregg@complete.tech>
2025-03-04 01:13:32 -0500

    Add Knowledge Graph adapter module for Research Orchestration Framework

---
+++ research_orchestrator/adapters/knowledge_graph_adapter/__init__.py
@@ -0,0 +1,6 @@
+"""
+Knowledge Graph System adapter for the Research Orchestration Framework.
+
+This module provides adapters for integrating the Knowledge Graph System
+with the Research Orchestration Framework.
+"""
