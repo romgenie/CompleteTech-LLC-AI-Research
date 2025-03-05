2668b44a9ed3d49a30d9287522e019b3eb008d23
Timothy Gregg <Timothy.Gregg@complete.tech>
2025-03-04 19:09:47 -0500

    feat: re-export hooks for easier imports

---
+++ src/ui/frontend/src/hooks/index.ts
@@ -0,0 +1,6 @@
+// Re-export hooks for easier imports
+export { default as useWebSocket } from './useWebSocket';
+export { default as useD3 } from './useD3';
+export { default as useFetch } from './useFetch';
+export { default as useLocalStorage } from './useLocalStorage';
+export { default as useErrorBoundary } from './useErrorBoundary';
